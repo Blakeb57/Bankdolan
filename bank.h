0ab375616548af4b89060e9b371d27093f1cfde8
Blakeb57 <bb148917@ohio.edu>
2021-01-25 19:38:58 -0500

    studying

--- bank.h
+++ bank.h
@@ -1,1 +1,32 @@
+#include <iostream>
+#include <string>
+#ifndef BANK_H
+#define BANK_H
 
+namespace dolan
+{
+    class Bankacct
+    {
+        public:
+            Bankacct(const std::string& id = "NA", double b = 0.0, double ir = 1.0);
+            //ACESSORS
+            std::string get_id() const{return ownerid;}
+            double get_balance() const{return balance;}
+            double get_interest() const{return interest_r;}
+            //SETTERS
+            //I/O
+            void input(std::istream& ins);
+            void output(std::ostream& outs)const;
+            //OPERATORS
+            Bankacct operator + (const Bankacct& b2)const;
+            Bankacct operator * (double amt)const;
+            bool operator < (const Bankacct& b2)const;
+        private:
+            std::string ownerid;
+            double balance;
+            double interest_r;
+    };
+    std::istream& operator >> (std::istream& ins, Bankacct& b2);
+    std::ostream& operator << (std::ostream& outs, const Bankacct& b2);
+} //CLOSING THE NAMESPACE
+#endif
--- main.cc
+++ main.cc
@@ -1,1 +1,30 @@
+/* 
+This is the main for the Bank pratice code
+*/
 
+#include <iostream>
+#include "bank.h"
+using namespace std;
+using dolan::Bankacct;
+
+int main()
+{
+    Bankacct mine ("ProfDolan"), yours, ours;
+    
+    mine.input(cin);
+    yours.input(cin);
+
+    ours = yours + mine;
+
+    Bankacct rich;
+    rich = yours * 2;
+
+    if(yours < mine)
+    {
+        cout << "I will buy lunch." << endl;
+    }
+    cout << mine << yours << endl;
+    cin >> rich >> mine;
+
+    ours = yours + mine + rich;
+}
